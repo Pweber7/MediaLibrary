67f222bda4432d26266ed253b79ff65711fa860f
phillip Weber <Pweber7@iastate.edu>
2016-04-03 14:36:01 -0500

    I made it work! Time to go home.

--- src/medialibrary/UserInterface.java
+++ src/medialibrary/UserInterface.java
@@ -1,9 +1,15 @@
 package medialibrary;
+import java.util.Scanner;
 
 public class UserInterface {
 	public static void main(String[] args)
 	{
-		
+		Scanner reader = new Scanner(System.in);
+		String action = "";
+		while(!action.toUpperCase().equals("QUIT"))
+		{
+			System.out.println("What would you like to do? ");
+			action = reader.nextLine();
+		}
 	}
-	
 }
