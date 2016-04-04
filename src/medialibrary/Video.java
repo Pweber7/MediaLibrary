1ac96178c76ea4cc169bc47c497738013a7ae022
Jacob Spoelstra <jacobs62@iastate.edu>
2016-04-04 16:54:20 -0500

    Added title and constructor to Video and added Movie and TVShow classes
    Movie, TVShow, and Video have the minimum work done in order for them to
    work with DatabaseSupport for now.

--- src/medialibrary/Video.java
+++ src/medialibrary/Video.java
@@ -2,4 +2,13 @@ package medialibrary;
 
 public abstract class Video {
 
+	private String vTitle;
+	
+	protected Video(String title){
+		vTitle = title;
+	}
+	
+	public String getTitle(){
+		return vTitle;
+	}
 }
