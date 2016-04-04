b9bf23a05e655caffde88c7f0c019e7e746879e0
Tyler Fenton <Tyler Fenton@10.26.1.233>
2016-04-04 11:48:38 -0500

    Created the constructor for Song.

--- src/medialibrary/Song.java
+++ src/medialibrary/Song.java
@@ -1,5 +1,9 @@
 package medialibrary;
 
 public class Song {
-
+	public Song(String title, String artist, String genera){
+		String sTitle = title;
+		String sArtist = artist;
+		String sGenera = genera;
+	}
 }
