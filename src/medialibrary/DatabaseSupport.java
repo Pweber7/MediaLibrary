9b5f032571f7829c51b0e17324bfb0a4e09e5a92
Jacob Spoelstra <jacobs62@iastate.edu>
2016-04-04 11:21:39 -0500

    Added skeletal DatabaseSupport class and Song and Video classes

---
+++ src/medialibrary/DatabaseSupport.java
@@ -0,0 +1,38 @@
+package medialibrary;
+
+import java.util.List;
+
+public class DatabaseSupport {
+
+	public List<Video> getWatchlist(){
+		return null;
+	}
+	
+	public List<Song> getPlaylist(){
+		return null;
+	}
+	
+	public boolean removeFromWatchlist(String title){
+		return false;
+	}
+	
+	public boolean removeFromPlaylst(String title){
+		return false;
+	}
+	
+	public List<Video> listVideoLibrary(){
+		return null;
+	}
+	
+	public List<Song> listMusicLibrary(){
+		return null;
+	}
+	
+	public Video getVideo(String title){
+		return null;
+	}
+	
+	public Song getSong(String title){
+		return null;
+	}
+}
---
+++ src/medialibrary/Video.java
@@ -0,0 +1,5 @@
+package medialibrary;
+
+public abstract class Video {
+
+}
