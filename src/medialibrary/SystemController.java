4e93245c23c36fa5707364581ad9cd80bb528329
Tyler Fenton <Tyler Fenton@10.26.204.235>
2016-04-06 11:46:08 -0500

    completed the first nine use cases for the SystemController class

--- src/medialibrary/SystemController.java
+++ src/medialibrary/SystemController.java
@@ -10,12 +10,34 @@ public class SystemController {
 	}
 	
 	public boolean removeFromWatchlist(String title){
-		return MLSystem.removeFromWatchlist(title);
+		return mls.removeFromWatchlist(title);
 	}
 	
 	public List<Video> listVideoLibrary(){
-		return MLSystem.listVideoLibrary;
+		return mls.listVideoLibrary();
 	}
 	
-	public
+	public Video getInformationVideo(String title){
+		return mls.getInformationVideo(title);
+	}
+	
+	public Song getInformationSong(String title){
+		return mls.getInformationSong(title);
+	}
+	
+	public boolean addToWatchlist(String title){
+		return mls.addToWatchlist(title);
+	}
+	
+	public List<Song> listMusicLibrary(){
+		return mls.listMusicLibrary();
+	}
+	
+	public List<Song> listPlaylist(){
+		return mls.listPlaylist();
+	}
+	
+	public boolean addToPlaylist(String title){
+		return mls.addToPlaylist(title);
+	}
 }
