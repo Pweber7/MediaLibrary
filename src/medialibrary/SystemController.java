7f5411c3f026dbd65586f1a4f79c491263f7a8cc
Tyler Fenton <Tyler Fenton@10.26.204.235>
2016-04-06 11:37:18 -0500

    Added removeFromWatchlist and listVideoLibrary

--- src/medialibrary/SystemController.java
+++ src/medialibrary/SystemController.java
@@ -7,4 +7,14 @@ public class SystemController {
 	public List<Video> listWatchlist(){
 		return MLSystem.listWatchlist();
 	}
+	
+	public boolean removeFromWatchlist(String title){
+		return MLSystem.removeFromWatchlist(title);
+	}
+	
+	public List<Video> listVideoLibrary(){
+		return MLSystem.listVideoLibrary;
+	}
+	
+	public
 }
